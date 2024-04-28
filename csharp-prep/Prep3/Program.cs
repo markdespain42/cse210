9e09961c5583681e142169155e622c37df6aeafe
markdespain42 <des21006@byui.edu>
2024-04-27 19:17:20 -0600

    Finished and solved all problems

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+        string response = "no";
+
+        while (response == "no")
+        {
+        Console.WriteLine("What is your guess (1-100)? ");
+        string newGuess = Console.ReadLine();
+        int newGuessNumber = int.Parse(newGuess);
+        if (magicNumber == newGuessNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            response = "yes";
+        }
+        else if (magicNumber > newGuessNumber)
+        {
+            Console.WriteLine("Higher");
+            response = "no";
+        }
+        else if (magicNumber < newGuessNumber)
+        {
+            Console.WriteLine("Lower");
+            response = "no";
+        }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int number = -1;
+        int total = 0;
+        int largestNumber = 0;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            int newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+            else 
+            {
+                number = 0;
+            }
+        }
+        foreach (int standardNumber in numbers)
+        {
+            Console.WriteLine(standardNumber);
+            total += standardNumber;
+            if (standardNumber > largestNumber)
+            {
+                largestNumber = standardNumber;
+            }
+
+        }
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
