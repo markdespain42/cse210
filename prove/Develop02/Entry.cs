d8d47bad1dccbc174f1209e285ee95bff57bc626
markdespain42 <des21006@byui.edu>
2024-05-03 00:35:06 -0600

    Created "stubs". Still some to go.

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Entry;
+{
+    public string _displayResponse; //void Display()
+    public string _date; //string DisplayDate()
+    public string _promptText; //string DisplayPrompt()
+    public string _entryText; //string DisplayEntryText()
+
+}
