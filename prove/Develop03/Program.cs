9c9ecca4187627e879ca8ca631ac5f1c73fb5ab4
markdespain42 <des21006@byui.edu>
2024-05-15 14:48:20 -0600

    Commiting and updating new content

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string scriptureText = "For God so loved the world that he gave his only begotten Son, that whosoever he had shall not perish but have everlasting life.";
+        Reference reference = new Reference("John 3:16");
+        reference.Display();
+
+        Scripture scripture = new Scripture(reference, scriptureText);
+        scripture.Display();
+
+        WordRandom wordRandom = new WordRandom();
+
+        for (int i = 0; i < wordRandom.txt.Length; i++)
+        {
+            string randomWord = wordRandom.GetRandomWord();
+            string underlinedWord = wordRandom.MakeUnderline(randomWord);
+
+            Console.WriteLine("Press any key to continue or 'q' to quit...");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == 'q')
+            {
+                break;
+            }
+
     }
+}
 }
