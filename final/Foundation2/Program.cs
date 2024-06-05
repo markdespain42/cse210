f988485a38b5aa130a8fb879d60abf60a4ae60f3
markdespain42 <des21006@byui.edu>
2024-06-05 13:04:05 -0600

    Pushing code to github

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Order order = new Order();
+        order.AddProduct("Bananas");
+        order.AddProduct("Apples");
+        Console.WriteLine(order.OrderList("John Smith"));
+        Console.WriteLine(order.OrderPrice(5));
+        Console.WriteLine(order.PackingLabel("Bananas", 1, 5, 2));
+        Console.WriteLine(order.ShippingLabel("John Smith", "123 Main Street", "Anytown", "CA", "USA"));
     }
 }
